b8ad71186e3f24a0cb4c25abf6a994c36980f112
Alex Stuart <alex@das.li>
2020-12-19 17:39:48 -0800

    Moved `Graphics.UI.Lightarrow` modules to new library

--- src/Lightarrow.hs
+++ src/Lightarrow.hs
@@ -1,13 +1,11 @@
 module Lightarrow (     module Data.Lightarrow,
                         module Graphics.Lightarrow,
-                        module Graphics.UI.Lightarrow,
                         module Simulation.Lightarrow,
                         module Sound.Lightarrow,
                         module System.Lightarrow    ) where
 
 import Data.Lightarrow
 import Graphics.Lightarrow
-import Graphics.UI.Lightarrow
 import Simulation.Lightarrow
 import Sound.Lightarrow
 import System.Lightarrow
