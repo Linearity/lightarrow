41155e2398e9f9eade9d6a4aceae69deda9d14ed
Alex Stuart <alex@das.li>
2021-07-07 15:32:11 -0700

    Add rotation parameter to `blit`

--- src/Graphics/Lightarrow/Blit.hs
+++ src/Graphics/Lightarrow/Blit.hs
@@ -12,7 +12,7 @@ class (BitmapPlatform m, ActuatePlatform m) => BlitPlatform m where
     
     Copy a bitmap to the screen.  The bitmap may be tinted; use 'White' for no
     tint.  Its position in the frame has three components: horizontal and
-    vertical offset of the upper-left corner from the center of the frame, and
+    vertical offset of the bitmap's center from the center of the frame, and
     depth.  Bitmaps are drawn in order of increasing depth value, that is
     bitmaps with greater depth are drawn over those with lesser depth.
 
@@ -20,17 +20,22 @@ class (BitmapPlatform m, ActuatePlatform m) => BlitPlatform m where
     blit    :: Bitmap m                             -- ^ the bitmap to copy
                     -> Color                        -- ^ tint
                     -> (Double, Double)             -- ^ horizontal and vertical scaling
+                    -> Double                       -- ^ counterclockwise rotation
                     -> (Double, Double, Double)     -- ^ position in frame
                     -> Actuation m                  -- ^ the copy command
 
 -- | An actuation that draws a bitmap, filtered by a given color, with
--- its center at the origin in a given transformed space and scaled by the
--- scaling part of the transformation
+-- its center at the origin in a given transformed space, rotated by the
+-- rotation part of the transformation, and scaled by the scaling part of
+-- the transformation
 sceneBlit :: BlitPlatform p =>
                 Bitmap p                        -- ^ the bitmap to draw
                     -> Color                    -- ^ tint
                     -> SceneTransform Double    -- ^ transformation
                     -> Actuation p              -- ^ the drawing command
-sceneBlit b c xf = blit b c (sX, sY) (x, y, z)
-    where   V3 x y z    = getTranslation (center b xf)
-            V3 sX sY _  = getScale (center b xf)
+sceneBlit b c xf = blit b c (sX, sY) r (x, y, z)
+    where   r           = unangle (V2 rX rY)
+            V3 x y z    = getTranslation xf
+            V3 rX rY _  = Linear.rotate (getRotation xf) (V3 1 0 0)
+            V3 sX sY _  = getScale xf
+            -- xf'         = center b xf
